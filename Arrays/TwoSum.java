ee316d5099f9468e544d4b3b5012889d422bb524
Vidhanshi Sharma <vidhanshisharma1707@gmail.com>
2025-10-08 10:47:09 +0530

    Add comments in TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -6,24 +6,26 @@ import java.util.HashMap;
 
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>(); // (value → index)
 
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
+            int complement = target - nums[i]; // number we need to find
 
             if (map.containsKey(complement)) {
                 return new int[] { map.get(complement), i };
             }
-            map.put(nums[i], i);
+
+            map.put(nums[i], i); // store current value and its index
         }
-        return new int[] {};
+
+        return new int[] {}; // return empty if no solution found
     }
 
     public static void main(String[] args) {
         int[] nums = { 2, 7, 11, 15 };
         int target = 9;
-        int[] result = twoSum(nums, target);
 
+        int[] result = twoSum(nums, target);
         System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
     }
 }
